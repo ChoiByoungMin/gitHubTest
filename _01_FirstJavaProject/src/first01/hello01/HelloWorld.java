449bf26e529e85b26569ac5ff15f4b46469deffc
Lenovo <Lenovo@DESKTOP-1L3HC4J>
2024-02-20 10:18:23 +0900

    HelloEveryOne 출력

--- _01_FirstJavaProject/src/first01/hello01/HelloWorld.java
+++ _01_FirstJavaProject/src/first01/hello01/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWord");
 		System.out.println("HelloJava");
+		System.out.println("HelloEveryOne");
 	}
 }
